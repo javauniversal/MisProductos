f128d2c5b2a29d33a10f20927fcb182555d3a730
German Garcia <ggarcia@intergrupo.com>
2020-05-29 00:49:43 -0500

    creating the mvp architecture structure

---
+++ app/src/main/java/com/fiiss/misproductos/interfaces/InterfaceActivity.java
@@ -0,0 +1,22 @@
+package com.fiiss.misproductos.interfaces;
+
+public interface InterfaceActivity {
+
+    interface View {
+        //void respuestaTokenUsuarioView(UsuarioApp usuarioApp);
+        //void respuestaTolenErrorView(String data);
+    }
+
+    interface Model {
+        //void getTokenUsuarioModel(UsuarioApp usuarioApp, ServiceRestApp serviceRestApp);
+        //void getUsuarioModel(UsuarioApp usuarioApp, ServiceRestApp serviceRestApp);
+    }
+
+    interface Presenter {
+        //void getTokenUsuarioPresenter(UsuarioApp usuarioApp, ServiceRestApp serviceRestApp);
+        //void respuestaTokenPresenter(UsuarioApp usuarioApp);
+        ///void respuestaErrorTokenPresenter(String data);
+        //void getUsuarioPresenter(UsuarioApp usuarioApp, ServiceRestApp serviceRestApp);
+    }
+
+}
--- app/src/main/java/com/fiiss/misproductos/view/ListProduct.java
+++ app/src/main/java/com/fiiss/misproductos/view/ListProduct.java
@@ -3,15 +3,50 @@ package com.fiiss.misproductos.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import com.fiiss.misproductos.R;
 
-public class ListProduct extends AppCompatActivity {
+public class ListProduct extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+
+    private Spinner spMaterial;
+    private Spinner spSaid;
+    private Spinner spType;
+    private RadioGroup radioGroup;
+    private Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spMaterial = findViewById(R.id.spMaterial);
+        spSaid = findViewById(R.id.spSaid);
+        spType = findViewById(R.id.spType);
+        radioGroup = findViewById(R.id.radioGroup);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnCalcular) {
+
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
