6501cdea964e9558a860f51ef75bca90b6f4d74d
German Garcia <ggarcia@intergrupo.com>
2020-05-28 23:49:14 -0500

    first commit

---
+++ app/src/main/java/com/fiiss/misproductos/view/ListProduct.java
@@ -0,0 +1,17 @@
+package com.fiiss.misproductos.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fiiss.misproductos.R;
+
+public class ListProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
